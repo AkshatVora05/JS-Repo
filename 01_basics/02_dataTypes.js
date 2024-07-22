637ea3bb4739db9374732fd9047292afa64a2512
AkshatVora05 <165830686+AkshatVora05@users.noreply.github.com>
2024-07-22 16:54:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+//but now it is already set to newer version so no need to use it
+
+// alert(3+3) this is not going to work as this is node.js not browser
+let name = "Akshat";
+let age = 20;
+let isLoggedIn = false;
+
+console.log(typeof 56);
+console.log(typeof "");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof name);
