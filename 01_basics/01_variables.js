f49ebe02c0d801aeda96d45aa8f87ec34f653323
AkshatVora05 <165830686+AkshatVora05@users.noreply.github.com>
2024-07-22 22:27:28 +0530

    Delete 01_basics/01_variables.js

--- 01_basics/01_variables.js
+++
@@ -1,9 +0,0 @@
-const accountId = 46;
-let accountEmail = "voraakshat05@gmail.com";
-var accountPswd = "12345678"
-accountCity = "GDGR"
-let accountState;
-console.log(accountEmail);
-console.log(accountPswd);
-console.log(accountId);
-console.table([accountId, accountEmail, accountPswd, accountCity, accountState]);
