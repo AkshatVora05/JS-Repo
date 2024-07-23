ae77e244e04dc6007d3c5cb06f4048f3a705d219
AkshatVora05 <165830686+AkshatVora05@users.noreply.github.com>
2024-07-23 14:21:16 +0000

    Comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1);
+
+// console.log(null > 0); // null is treated as 0
+// console.log(null == 0); // null is not treated as 0
+// console.log(null >= 0); // null is treated as 0
+
+// strictly check ===
+
+console.log("2" == 2);
+console.log("2" === 2);
