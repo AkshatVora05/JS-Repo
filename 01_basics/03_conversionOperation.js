d2c6d356eda8b11e94edddbc99007b41937d9f81
AkshatVora05 <165830686+AkshatVora05@users.noreply.github.com>
2024-07-23 13:27:18 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score = "33";
+// let score1 = "33abc";
+// let score2 = null;
+// let score3 = undefined;
+
+// console.log(typeof score);
+// console.log(typeof score1);
+// console.log(typeof score2);
+// console.log(typeof score3);
+
+// let num = Number(score);
+
+// let num1 = Number(score1);
+
+// let num2 = Number(score2);
+
+// let num3 = Number(score3);
+
+// console.log(num);
+// console.log(typeof num);
+
+// console.log(num1);
+// console.log(typeof num1);
+
+// console.log(num2);
+// console.log(typeof num2);
+
+// console.log(num3);
+// console.log(typeof num3);
+
+// "33" - 33
+// "33abc" - NaN
+// true - 1
+// null - 0
+// undefined - NaN
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// "" - false
+// not an empty string - true
+// 0 - false
+// any other number - true
+
+let numb = -33;
+let stringNumber = String(numb);
+console.log(stringNumber);
