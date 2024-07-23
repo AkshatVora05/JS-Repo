25a7c3653bfffe7bc64ad7fac254ed919807dd1b
AkshatVora05 <165830686+AkshatVora05@users.noreply.github.com>
2024-07-23 14:07:59 +0000

    Conversion and Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -45,6 +45,30 @@
 // 0 - false
 // any other number - true
 
-let numb = -33;
-let stringNumber = String(numb);
-console.log(stringNumber);
+// let numb = -33;
+// let stringNumber = String(numb);
+// console.log(stringNumber);
+
+/**********************************OPERATIONS********************/
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " World";
+
+str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
