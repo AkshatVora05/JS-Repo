033634ce7ddfd22af36cba3f38f9318ebd6793e0
AkshatVora05 <165830686+AkshatVora05@users.noreply.github.com>
2024-07-29 18:17:29 +0000

    String operations

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+// // One way to declare a string
+// const name = "Akshat";
+// const id  = 5;
+// //console.log(name + id + "@gmail.com"); this is outdated syntax
+
+// console.log(`My name is ${name} and my id is ${id}`);
+
+// Second way to declare a string
+// const name = new String("Akshat");
+// console.log(name.length);
+// console.log(name[0]);
+// console.log(name.__proto__);
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.charAt(2));
+// console.log(name.substring(0, 4));
+
+// const newStr = "   string    ";
+// console.log(newStr.trim());
+
+// console.log(newStr.replace("st", "").trim());
+// console.log(newStr.includes("st"));
+
+const name = new String("Akshat-Vora");
+console.log(name.split('-'));
